8e60d61c5aed1fd38ddebfad3cb9fafa01cb187d
Menna Abdallah <139376864+menna-abdallah@users.noreply.github.com>
2024-02-15 22:12:12 +0200

    Update todos.js

--- Lab 5/routes/todos.js
+++ Lab 5/routes/todos.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const TodosController = require('../controllers/todocontroller');
+const asyncWrapper = require ('../lib/async-wrapper')
 
 const router = express.Router();
 
-const auth = require ('../middlewares/auth')
+const auth = require('../middlewares/auth');
 
-// ------------------------- POST /todos -----------------
-/*
-// private rout // middleware // verfy jwt
-router.use(auth)
+
+router.use(auth);
+
 router.post('/', async (req, res, next) => {
-  const { body: { title } } = req;
-  const todo =await TodosController.createtodo({ title, userId: req.user._id });
-  res.json(todo);
+  req.body.userId = req.user;
+  const [err, todo] = await asyncWrapper(TodosController.createtodo(req.body));
+  if (!err) {
+    return res.json(todo);
+  }
+  return next(err);
 });
-*/
-// ------------------------- DELETE /todos/:id -----------------
 
-router.delete('/:id', TodosController.deletetodo);
+router.patch('/:id', async (req, res, next) => {
+  const [err, todo] = 
+  await asyncWrapper(TodosController.updateTodo(req.params.id, req.body, req.user));
+  if (!err) {
+    return res.json(todo);
+  }
+  return next(err);
+});
 
-// ------------------------- Patch/todos/:id -----------------
 
-router.patch('/:id', TodosController.updatetodo);
+router.delete('/:id', async (req, res, next) => {
+  const [err, todo] = await asyncWrapper(TodosController.deletetodo(req.params.id));
+  if (!err) {
+    return res.json(todo);
+  }
+  return next(err);
+});
+
+/*
 
 // -------------------------- get /todos?limit=10&skip=0&status=$value --------------
 
 router.get('/', TodosController.filterTodo);
 // ------------------------- Export -----------------
+*/
 module.exports = router;
